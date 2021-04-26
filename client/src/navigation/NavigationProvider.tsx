3c628eef77452e9aa77a7337067e7fd93204b4be
ItCpu019 <>
2021-04-26 18:52:20 +0530

    TSFixMe 1

---
+++ client/src/navigation/NavigationProvider.tsx
@@ -0,0 +1,71 @@
+import {
+  DownloadsScreen,
+  HomeScreen,
+  ClassScreen,
+  MeditationScreen,
+  CourseScreen,
+  ArticleScreen,
+  ClassPlayerScreen,
+  CoursePlayerScreen,
+  ArticlePlayerScreen,
+  MeditationPlayerScreen
+} from "../screens";
+import { ErrorMessage } from "../components";
+import { Favorite } from '../screens/FavoritesScreen/types'
+import React, { createContext, useState, FunctionComponent } from "react";
+
+type TSFixMe = {
+  [key: string]: FunctionComponent<any>;
+}
+
+type RouteParams = {
+  content?: Favorite;
+  id?: string;
+}
+
+type InitialRouteType = {
+  route: string;
+  params?: RouteParams;
+}
+
+const routes: TSFixMe = {
+  HomeScreen,
+  DownloadsScreen,
+  ClassScreen,
+  MeditationScreen,
+  CourseScreen,
+  ArticleScreen,
+  ClassPlayerScreen,
+  CoursePlayerScreen,
+  ArticlePlayerScreen,
+  MeditationPlayerScreen
+};
+const initialRoute = { route: "HomeScreen", params: {} };
+
+export const NavigationContext = createContext<{
+  activeRoute: InitialRouteType;
+  setActiveRoute: (a: InitialRouteType) => void;
+}>({
+  activeRoute: initialRoute,
+  setActiveRoute: () => console.warn("Missing navigation provider")
+});
+
+export const NavigationProvider: React.FC = ({ children }) => {
+  const [activeRoute, setActiveRoute] = useState<InitialRouteType>(initialRoute);
+  const ScreenComponent = routes[activeRoute.route];
+  if (!ScreenComponent) return <ErrorMessage msg="Missing ScreenComponent!" />;
+
+  return (
+    <NavigationContext.Provider
+      value={{
+        activeRoute,
+        setActiveRoute
+      }}
+    >
+      {children}
+      <div style={{ padding: 30 }}>
+        <ScreenComponent {...activeRoute.params} />
+      </div>
+    </NavigationContext.Provider>
+  );
+};
